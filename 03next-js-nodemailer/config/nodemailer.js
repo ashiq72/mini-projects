01a1ec209ea586263ab303d6e5154e81b52f9adf
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-10-18 09:15:01 +0600

    nodemailer added

--- 03next-js-nodemailer/config/nodemailer.js
+++ 03next-js-nodemailer/config/nodemailer.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
-
+// lmvydrnfrmttmfcg
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
