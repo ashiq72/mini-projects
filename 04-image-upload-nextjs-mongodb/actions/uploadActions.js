ee5f3bea09e60043afe7932cee2c61ead4b02966
Md ashiquzzaman <ahmedashik18k@gmail.com>
2023-11-01 10:14:28 +0600

    drag drop image upload local initial

--- 04-image-upload-nextjs-mongodb/actions/uploadActions.js
+++ 04-image-upload-nextjs-mongodb/actions/uploadActions.js
@@ -80,11 +80,19 @@ export async function uploadPhoto(formData) {
 
 export async function getAllPhotos() {
   try {
-    const { resources } = await cloudinary.v2.search
-      .expression("folder: nextjs_upload/*")
-      .sort_by("created_at", "desc")
-      .max_results(500)
-      .execute();
+    //From cloudinary database
+    // const { resources } = await cloudinary.v2.search
+    //   .expression("folder: nextjs_upload/*")
+    //   .sort_by("created_at", "desc")
+    //   .max_results(500)
+    //   .execute();
+
+    //From Mongodb
+    const photos = await Photo.find().sort("-createdAt");
+    const resources = photos.map((photo) => ({
+      ...photo._doc,
+      _id: photo._id.toString(),
+    }));
     return resources;
   } catch (error) {
     return { errMsg: error.message };
